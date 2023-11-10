5729a8d37c72587ba36913113cde04dde260d9a6
Christian Benincasa <chrisbenincasa@gmail.com>
2023-11-10 10:04:01 -0500

    Basic channels page

--- web2/src/App.tsx
+++ web2/src/App.tsx
@@ -2,50 +2,28 @@ import MenuIcon from '@mui/icons-material/Menu';
 import {
   AppBar,
   Box,
+  Button,
   IconButton,
-  Link,
   Toolbar,
   Typography,
 } from '@mui/material';
 import Container from '@mui/material/Container';
 import CssBaseline from '@mui/material/CssBaseline';
 import { useQuery } from '@tanstack/react-query';
-import { Outlet, Link as RouteLink } from '@tanstack/react-router';
+import { Outlet } from '@tanstack/react-router';
 import { useState } from 'react';
 import './App.css';
-import LinkRouter from './components/LinkRouter.tsx';
+import LinkRouter from './components/LinkRouter';
+import TextSnippetIcon from '@mui/icons-material/TextSnippet';
 
 export function Root() {
-  return (
-    <>
-      <div>
-        <LinkRouter to={'/'}>Home</LinkRouter>
-        <LinkRouter to="/channels">Channels</LinkRouter>
-        <Outlet />
-      </div>
-    </>
-  );
-}
-
-function App() {
   const [_, setOpen] = useState(true);
   const toggleDrawer = () => {
     setOpen(!true);
   };
-  const { isPending, error, data } = useQuery({
-    queryKey: ['test'],
-    queryFn: () =>
-      fetch('http://localhost:8000/api/channels').then((res) => res.json()),
-  });
-
-  if (isPending) return 'Loading...';
-
-  if (error) return 'An error occurred!: ' + error.message;
-
-  console.log(data);
 
   return (
-    <>
+    <div>
       <Box sx={{ display: 'flex' }}>
         <CssBaseline />
 
@@ -69,16 +47,46 @@ function App() {
             >
               DizqueTV
             </Typography>
+            <Button
+              href="/api/xmltv.xml"
+              color="inherit"
+              startIcon={<TextSnippetIcon />}
+            >
+              XMLTV
+            </Button>
+            <Button
+              href="/api/channels.m3u"
+              color="inherit"
+              startIcon={<TextSnippetIcon />}
+            >
+              M3U
+            </Button>
           </Toolbar>
         </AppBar>
-        <Container>
-          <Link component={RouteLink} to={'/channels'}>
-            Test
-          </Link>
-        </Container>
+        <Box
+          component="main"
+          sx={{
+            backgroundColor: (theme) =>
+              theme.palette.mode === 'light'
+                ? theme.palette.grey[100]
+                : theme.palette.grey[900],
+            flexGrow: 1,
+            height: '100vh',
+            overflow: 'auto',
+          }}
+        >
+          <Toolbar />
+          <Container maxWidth="xl" sx={{ mt: 4, mb: 4 }}>
+            <Outlet />
+          </Container>
+        </Box>
       </Box>
-    </>
+    </div>
   );
+}
+
+function App() {
+  return <LinkRouter to="/channels">Channels</LinkRouter>;
 }
 
 export default App;
--- web2/src/pages/channels/ChannelsPage.tsx
+++ web2/src/pages/channels/ChannelsPage.tsx
@@ -1,9 +1,78 @@
-import { Typography } from '@mui/material';
+import {
+  IconButton,
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+} from '@mui/material';
+import { useQuery } from '@tanstack/react-query';
+import DeleteIcon from '@mui/icons-material/Delete';
 
 export default function ChannelsPage() {
+  const { isPending, error, data } = useQuery({
+    queryKey: ['test'],
+    queryFn: () =>
+      fetch('http://localhost:8000/api/channels').then((res) => res.json()),
+  });
+
+  if (isPending) return 'Loading...';
+
+  if (error) return 'An error occurred!: ' + error.message;
+
+  // TODO properly define types from API
+  const getDataTableRow = (channel) => {
+    return (
+      <TableRow key={channel.number}>
+        <TableCell width="10%">{channel.number}</TableCell>
+        <TableCell width="10%">
+          <img style={{ maxHeight: '40px' }} src={channel.icon.path} />
+        </TableCell>
+        <TableCell>{channel.name}</TableCell>
+        <TableCell width="10%">
+          <IconButton color="error">
+            <DeleteIcon />
+          </IconButton>
+        </TableCell>
+      </TableRow>
+    );
+  };
+
+  const getTableRows = () => {
+    if (isPending) {
+      return (
+        <TableRow key="pending">
+          <TableCell colSpan={4}>Loading....</TableCell>
+        </TableRow>
+      );
+    } else if (error) {
+      return (
+        <TableRow key="pending">
+          <TableCell colSpan={4}>Error</TableCell>
+        </TableRow>
+      );
+    } else {
+      return data.map(getDataTableRow);
+    }
+  };
+
   return (
     <div>
-      <Typography>Channels</Typography>
+      <TableContainer component={Paper}>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>Number</TableCell>
+              <TableCell>Icon</TableCell>
+              <TableCell>Name</TableCell>
+              <TableCell></TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>{getTableRows()}</TableBody>
+        </Table>
+      </TableContainer>
     </div>
   );
 }
