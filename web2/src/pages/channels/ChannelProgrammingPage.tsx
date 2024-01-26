c31993b9498f60fd6889047b7b8e074b3e0bc24b
Mark D'Avella <markdavella@gmail.com>
2024-01-26 13:14:09 -0500

    Added page to handle no channels

--- web2/src/pages/channels/ChannelProgrammingPage.tsx
+++ web2/src/pages/channels/ChannelProgrammingPage.tsx
@@ -1,4 +1,11 @@
-import { Box, Button, CircularProgress, Typography } from '@mui/material';
+import React from 'react';
+import {
+  Box,
+  Button,
+  CircularProgress,
+  Snackbar,
+  Typography,
+} from '@mui/material';
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { ZodiosError } from '@zodios/core';
 import { ChannelProgram } from '@tunarr/types';
@@ -8,13 +15,28 @@ import { ChannelProgrammingConfig } from '../../components/channel_config/Channe
 import { apiClient } from '../../external/api.ts';
 import { usePreloadedChannel } from '../../hooks/usePreloadedChannel.ts';
 import { setCurrentLineup } from '../../store/channelEditor/actions.ts';
+import { useTheme } from '@mui/material/styles';
 
 type MutateArgs = { channelNumber: number; newLineup: ChannelProgram[] };
+
+type SnackBar = {
+  display: boolean;
+  message: string;
+  color: string;
+};
 
 export default function ChannelProgrammingPage() {
   const { currentEntity: channel, programList: newLineup } =
     usePreloadedChannel();
+
   const queryClient = useQueryClient();
+  const theme = useTheme();
+
+  const [snackStatus, setSnackStatus] = React.useState<SnackBar>({
+    display: false,
+    color: '',
+    message: '',
+  });
 
   // TODO we need to update the channel start time too
   const updateLineupMutation = useMutation({
@@ -26,17 +48,31 @@ export default function ChannelProgrammingPage() {
     },
     onSuccess: async (data, { channelNumber }) => {
       setCurrentLineup(data.programs, /*dirty=*/ false);
+      setSnackStatus({
+        display: true,
+        message: 'Programs Saved!',
+        color: theme.palette.success.main,
+      });
       await queryClient.invalidateQueries({
         queryKey: ['channels', channelNumber],
       });
     },
     onError: (error) => {
+      setSnackStatus({
+        display: true,
+        message: error.message,
+        color: theme.palette.error.main,
+      });
       if (error instanceof ZodiosError) {
         console.error(error.data);
         console.error(error, error.cause);
       }
     },
   });
+
+  const handleSnackClose = () => {
+    setSnackStatus({ display: false, message: '', color: '' });
+  };
 
   const onSave = () => {
     updateLineupMutation
@@ -51,6 +87,14 @@ export default function ChannelProgrammingPage() {
     </div>
   ) : (
     <div>
+      <Snackbar
+        open={snackStatus.display}
+        autoHideDuration={6000}
+        anchorOrigin={{ vertical: 'bottom', horizontal: 'center' }}
+        onClose={handleSnackClose}
+        message={snackStatus.message}
+        sx={{ backgroundColor: snackStatus.color }}
+      />
       <Typography variant="h4" sx={{ mb: 2 }}>
         Channel {channel.number} Programming
       </Typography>
--- web2/src/pages/library/LibraryIndexPage.tsx
+++ web2/src/pages/library/LibraryIndexPage.tsx
@@ -7,7 +7,6 @@ import {
   Typography,
 } from '@mui/material';
 import { Link } from 'react-router-dom';
-import PaddedPaper from '../../components/base/PaddedPaper.tsx';
 import { ArrowForward } from '@mui/icons-material';
 
 export default function LibraryIndexPage() {
