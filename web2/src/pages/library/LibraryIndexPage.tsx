a02e02432fe9b1bbbf81f3e295d1e51d464842f7
Mark D'Avella <markdavella@gmail.com>
2024-01-26 11:16:20 -0500

    Collapsed Nav, Mobile Bar, & More UX Tweaks (#10)
    * additional tweaks
    
    * lock file update

--- web2/src/pages/library/LibraryIndexPage.tsx
+++ web2/src/pages/library/LibraryIndexPage.tsx
@@ -1,40 +1,79 @@
-import { Box, Stack, Typography } from '@mui/material';
+import {
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  Grid,
+  Typography,
+} from '@mui/material';
 import { Link } from 'react-router-dom';
 import PaddedPaper from '../../components/base/PaddedPaper.tsx';
+import { ArrowForward } from '@mui/icons-material';
 
 export default function LibraryIndexPage() {
   return (
-    <Box sx={{ width: '100%' }}>
-      <Stack direction="column" spacing={2}>
-        <PaddedPaper
-          component={Link}
-          to="/library/filler"
-          sx={{ textDecoration: 'none' }}
-        >
-          <Typography variant="h5" mb={1}>
-            Filler&hellip;
-          </Typography>
-          <Typography variant="body1">
-            Filler lists are collections of videos that you may want to play
-            during 'flex' time segments. Flex is time within a channel that does
-            not have a program scheduled (usually used for padding).
-          </Typography>
-        </PaddedPaper>
-        <PaddedPaper
-          component={Link}
-          to="/library/custom-shows"
-          sx={{ textDecoration: 'none' }}
-        >
-          <Typography variant="h5" mb={1}>
-            Custom Shows&hellip;
-          </Typography>
-          <Typography variant="body1">
-            Custom Shows are sequences of videos that represent a episodes of a
-            virtual TV show. When you add these shows to a channel, the schedule
-            tools will treat the videos as if they belonged to a single TV show.
-          </Typography>
-        </PaddedPaper>
-      </Stack>
-    </Box>
+    <>
+      <Typography variant="h3" mb={2}>
+        Library
+      </Typography>
+      <Grid
+        container
+        columns={{ sm: 8, md: 16 }}
+        columnSpacing={2}
+        rowSpacing={2}
+      >
+        <Grid item sm={16} md={8}>
+          <Card sx={{ minWidth: 275, pb: 1, pr: 1 }}>
+            <CardContent>
+              <Typography variant="h5" component="div">
+                Filler
+              </Typography>
+              <Typography variant="body2">
+                Filler lists are collections of videos that you may want to play
+                during 'flex' time segments. Flex is time within a channel that
+                does not have a program scheduled (usually used for padding).
+              </Typography>
+            </CardContent>
+            <CardActions sx={{ justifyContent: 'right' }}>
+              <Button
+                size="small"
+                variant="contained"
+                component={Link}
+                to="/library/filler"
+                endIcon={<ArrowForward />}
+              >
+                Edit Fillers
+              </Button>
+            </CardActions>
+          </Card>
+        </Grid>
+        <Grid item sm={16} md={8}>
+          <Card sx={{ minWidth: 275, pb: 1, pr: 1 }}>
+            <CardContent>
+              <Typography variant="h5" component="div">
+                Custom Shows
+              </Typography>
+              <Typography variant="body2">
+                Custom Shows are sequences of videos that represent a episodes
+                of a virtual TV show. When you add these shows to a channel, the
+                schedule tools will treat the videos as if they belonged to a
+                single TV show.
+              </Typography>
+            </CardContent>
+            <CardActions sx={{ justifyContent: 'right' }}>
+              <Button
+                size="small"
+                variant="contained"
+                component={Link}
+                to="/library/custom-shows"
+                endIcon={<ArrowForward />}
+              >
+                Edit Custom Shows
+              </Button>
+            </CardActions>
+          </Card>
+        </Grid>
+      </Grid>
+    </>
   );
 }
