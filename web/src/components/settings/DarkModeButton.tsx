7d2168945488ae0efe66640b4d8f77f533978428
Mark D'Avella <markdavella@gmail.com>
2024-03-08 12:41:55 -0500

    add dark mode to top bar

--- web/src/components/settings/DarkModeButton.tsx
+++ web/src/components/settings/DarkModeButton.tsx
@@ -1,18 +1,30 @@
-import { FormControlLabel, Switch } from '@mui/material';
-import { useStore } from 'zustand';
+import { DarkMode, LightMode } from '@mui/icons-material';
+import { FormControlLabel, IconButton, Switch } from '@mui/material';
+import useStore from '../../store/index.ts';
 import { setDarkModeState } from '../../store/themeEditor/actions';
 
-export default function DarkModeButton() {
+type DarkModeProps = {
+  iconOnly?: boolean;
+};
+
+export default function DarkModeButton(props: DarkModeProps) {
+  const { iconOnly } = props;
   const darkMode = useStore((state) => state.theme.darkMode);
 
   return (
     <>
-      <FormControlLabel
-        control={
-          <Switch checked={darkMode} onClick={() => setDarkModeState()} />
-        }
-        label="Dark Mode"
-      />
+      {iconOnly ? (
+        <IconButton onClick={() => setDarkModeState()} sx={{ mx: 1 }}>
+          {darkMode ? <DarkMode /> : <LightMode sx={{ color: '#fff' }} />}
+        </IconButton>
+      ) : (
+        <FormControlLabel
+          control={
+            <Switch checked={darkMode} onClick={() => setDarkModeState()} />
+          }
+          label="Dark Mode"
+        />
+      )}
     </>
   );
 }
