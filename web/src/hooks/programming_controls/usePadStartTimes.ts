e2f94c20877ef10c39d23471526c8a6a84327f75
Christian Benincasa <chrisbenincasa@gmail.com>
2024-03-22 07:21:53 -0400

    Add basic padding times (#203)
    Does not yet add support for "*0 or *5" and other similar paddings that
    dizque has, since that will require a bit more work.

--- web/src/hooks/programming_controls/usePadStartTimes.ts
+++ web/src/hooks/programming_controls/usePadStartTimes.ts
@@ -19,8 +19,12 @@ const OneMinuteMillis = 1000 * 60;
 
 export const StartTimePaddingOptions: readonly StartTimePadding[] = [
   { mod: -1, description: 'None' },
+  { mod: 5, description: ':05, :10, ..., :55' },
+  { mod: 10, description: ':10, :20, ..., :50' },
   { mod: 15, description: ':00, :15, :30, :45' },
   { mod: 30, description: ':00, :30' },
+  { mod: 60, description: ':00' },
+  { mod: 20, description: ':20, :40, :00' },
 ] as const;
 
 export function usePadStartTimes() {
