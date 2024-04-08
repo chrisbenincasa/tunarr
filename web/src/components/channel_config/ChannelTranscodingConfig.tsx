677b9eaede099e0faa303cfd7ac168dc4ca665c6
Christian Benincasa <chrisbenincasa@gmail.com>
2024-04-08 16:35:52 -0400

    Checkpoint - watermark config for channel w/ preview

--- web/src/components/channel_config/ChannelTranscodingConfig.tsx
+++ web/src/components/channel_config/ChannelTranscodingConfig.tsx
@@ -2,25 +2,33 @@ import {
   CircularProgress,
   Divider,
   FormControl,
+  FormControlLabel,
+  FormHelperText,
   Input,
   InputLabel,
   MenuItem,
   Select,
   SelectChangeEvent,
   Skeleton,
+  Stack,
   TextField,
 } from '@mui/material';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import { SaveChannelRequest } from '@tunarr/types';
-import { isNil, isUndefined, map } from 'lodash-es';
-import { useState } from 'react';
+import { isEmpty, isNil, isUndefined, map, round } from 'lodash-es';
+import { useEffect, useState } from 'react';
 import { Controller, useFormContext } from 'react-hook-form';
 import {
   resolutionFromAnyString,
   resolutionToString,
+  typedProperty,
 } from '../../helpers/util.ts';
 import { useFfmpegSettings } from '../../hooks/settingsHooks.ts';
+import { CheckboxFormController } from '../util/TypedController.tsx';
+import { ImageUploadInput } from '../settings/ImageUploadInput.tsx';
+import Grid2 from '@mui/material/Unstable_Grid2/Grid2';
+import useStore from '../../store/index.ts';
 
 const resolutionOptions = [
   { value: '420x420', label: '420x420 (1:1)' },
@@ -49,9 +57,24 @@ const globalOrNumber = /^(global|\d+|)+$/;
 export default function ChannelTranscodingConfig() {
   const { data: ffmpegSettings, isPending: ffmpegSettingsLoading } =
     useFfmpegSettings();
+  const channel = useStore((s) => s.channelEditor.currentEntity);
+  const [watermarkPreviewUrl, setWatermarkPreviewUrl] = useState('');
 
   const { control, watch, setValue } = useFormContext<SaveChannelRequest>();
-  const targetRes = watch('transcoding.targetResolution');
+
+  const [targetRes, watermark] = watch([
+    'transcoding.targetResolution',
+    'watermark',
+  ]);
+
+  useEffect(() => {
+    if (channel && (isNil(watermark?.url) || isEmpty(watermark?.url))) {
+      setWatermarkPreviewUrl(
+        !isEmpty(channel.icon.path) ? channel.icon.path : '/tunarr.png',
+      );
+    }
+  }, [channel, watermark?.url, setWatermarkPreviewUrl]);
+
   const [targetResString, setTargetResString] =
     useState<ResolutionOptionValues>(() => {
       if (isUndefined(targetRes) || targetRes === 'global') {
@@ -81,6 +104,14 @@ export default function ChannelTranscodingConfig() {
     ...resolutionOptions,
   ];
 
+  const targetResForPreview = (targetRes === 'global'
+    ? ffmpegSettings?.targetResolution
+    : targetRes) ?? { widthPx: 1920, heightPx: 1080 };
+  const paddingPct = round(
+    100 * (targetResForPreview.heightPx / targetResForPreview.widthPx),
+    2,
+  );
+
   const handleResolutionChange = (
     e: SelectChangeEvent<ResolutionOptionValues>,
   ) => {
@@ -97,100 +128,261 @@ export default function ChannelTranscodingConfig() {
   };
 
   return (
-    <Box sx={{ flex: 1, display: 'flex', flexDirection: 'column' }}>
-      <Box>
-        <Typography>Watermark</Typography>
-      </Box>
-      <Divider sx={{ my: 2 }} />
-      <Typography>Transcoding Settings</Typography>
-      <Typography variant="caption">
-        Use these settings to override global ffmpeg settings for this channel.
-      </Typography>
-      <Box sx={{ flex: 1 }}>
-        <FormControl margin="normal">
-          <InputLabel>Channel Resolution</InputLabel>
-          {ffmpegSettingsLoading ? (
-            <Skeleton>
-              <Input />
-            </Skeleton>
-          ) : (
-            <Controller
-              control={control}
-              name="transcoding.targetResolution"
-              render={() => (
-                <Select<ResolutionOptionValues>
-                  disabled={
-                    isNil(ffmpegSettings) || !ffmpegSettings.enableTranscoding
-                  }
-                  label="Channel Resolution"
-                  value={targetResString}
-                  onChange={(e) => handleResolutionChange(e)}
+    channel && (
+      <Box sx={{ flex: 1, display: 'flex', flexDirection: 'column' }}>
+        <Box>
+          <Typography>Watermark</Typography>
+          <FormControl fullWidth>
+            <FormControlLabel
+              control={
+                <CheckboxFormController
+                  control={control}
+                  name="watermark.enabled"
+                />
+              }
+              label="Enable Watermark"
+            />
+            <FormHelperText>
+              Renders a channel icon (also known as bug or Digital On-screen
+              Graphic) on top of the channel's stream.
+            </FormHelperText>
+          </FormControl>
+          {watermark?.enabled && (
+            <Stack direction="row" mt={2} gap={2} useFlexGap>
+              <Box sx={{ minWidth: '33%' }}>
+                <Box
+                  sx={{
+                    width: '100%',
+                    padding: `0 0 ${paddingPct}%`,
+                    position: 'relative',
+                    backgroundColor: (theme) => theme.palette.grey[200],
+                    borderColor: (theme) => theme.palette.grey[700],
+                    borderStyle: 'solid',
+                    borderWidth: 2,
+                    overflow: 'hidden',
+                  }}
                 >
-                  {allResolutionOptions.map((opt) => (
-                    <MenuItem key={opt.value} value={opt.value}>
-                      {opt.label}
-                    </MenuItem>
-                  ))}
-                </Select>
-              )}
-            />
+                  <Box
+                    component="img"
+                    sx={{
+                      position: 'absolute',
+                      width:
+                        watermark?.width && !watermark?.fixedSize
+                          ? `${watermark.width}%`
+                          : null,
+                      bottom: watermark?.verticalMargin,
+                      right: watermark?.horizontalMargin,
+                    }}
+                    src={watermarkPreviewUrl}
+                  />
+                </Box>
+              </Box>
+              <Grid2
+                container
+                spacing={2}
+                sx={{ flexGrow: 1, height: 'fit-content' }}
+              >
+                <Grid2 xs={12}>
+                  <Controller
+                    name="watermark.url"
+                    control={control}
+                    render={({ field }) => (
+                      <ImageUploadInput
+                        // TODO: This should be something like {channel.id}_fallback_picture.ext
+                        fileRenamer={typedProperty('name')}
+                        label="Watermark Picture URL"
+                        onFormValueChange={(value) =>
+                          setValue('watermark.url', value)
+                        }
+                        onUploadError={console.error}
+                        onPreviewValueChange={() => {}}
+                        FormControlProps={{ fullWidth: true, sx: { mb: 1 } }}
+                        value={field.value ?? ''}
+                      />
+                    )}
+                  />
+                </Grid2>
+                <Grid2 xs={12}>
+                  <Controller
+                    name="watermark.position"
+                    control={control}
+                    render={({ field }) => (
+                      <TextField
+                        sx={{ mb: 1 }}
+                        fullWidth
+                        label="Position"
+                        {...field}
+                      />
+                    )}
+                  />
+                </Grid2>
+                <Grid2 xs={12} sm={4}>
+                  <Controller
+                    control={control}
+                    name="watermark.width"
+                    render={({ field }) => (
+                      <TextField fullWidth label="Width %" {...field} />
+                    )}
+                  />
+                </Grid2>
+                <Grid2 xs={12} sm={4}>
+                  <Controller
+                    control={control}
+                    name="watermark.horizontalMargin"
+                    render={({ field }) => (
+                      <TextField
+                        fullWidth
+                        label="Horizontal Margin %"
+                        {...field}
+                      />
+                    )}
+                  />
+                </Grid2>
+                <Grid2 xs={12} sm={4}>
+                  <Controller
+                    control={control}
+                    name="watermark.verticalMargin"
+                    render={({ field }) => (
+                      <TextField
+                        fullWidth
+                        label="Vertical Margin %"
+                        {...field}
+                      />
+                    )}
+                  />
+                </Grid2>
+                <Grid2>
+                  <FormControl fullWidth>
+                    <FormControlLabel
+                      control={
+                        <CheckboxFormController
+                          control={control}
+                          name="watermark.fixedSize"
+                        />
+                      }
+                      label="Disable Image Scaling"
+                    />
+                    <FormHelperText>
+                      The image will be rendered at its actual size without
+                      applying any scaling to it.
+                    </FormHelperText>
+                  </FormControl>
+                </Grid2>
+                <Grid2>
+                  <FormControl fullWidth>
+                    <FormControlLabel
+                      control={
+                        <CheckboxFormController
+                          control={control}
+                          name="watermark.animated"
+                        />
+                      }
+                      label="Enable Animation"
+                    />
+                    <FormHelperText>
+                      Enable if the watermark is an animated GIF or PNG. The
+                      watermark will loop or not loop according to the image's
+                      configuration. If this option is enable and the image is
+                      not animated, there will be playback errors.
+                    </FormHelperText>
+                  </FormControl>
+                </Grid2>
+              </Grid2>
+            </Stack>
           )}
-        </FormControl>
-        <FormControl margin="normal">
-          {ffmpegSettingsLoading ? (
-            <Skeleton>
-              <Input />
-            </Skeleton>
-          ) : (
-            <Controller
-              control={control}
-              name="transcoding.videoBitrate"
-              disabled={
-                isNil(ffmpegSettings) || !ffmpegSettings.enableTranscoding
-              }
-              rules={{ pattern: globalOrNumber }}
-              render={({ field, formState: { errors } }) => (
-                <TextField
-                  label="Video Bitrate (kbps)"
-                  helperText={
-                    errors.transcoding && errors.transcoding.videoBitrate
-                      ? "Must be a number or 'global'"
-                      : null
-                  }
-                  {...field}
-                />
-              )}
-            />
-          )}
-        </FormControl>
-        <FormControl margin="normal">
-          {ffmpegSettingsLoading ? (
-            <Skeleton>
-              <Input />
-            </Skeleton>
-          ) : (
-            <Controller
-              control={control}
-              name="transcoding.videoBufferSize"
-              disabled={
-                isNil(ffmpegSettings) || !ffmpegSettings.enableTranscoding
-              }
-              rules={{ pattern: globalOrNumber }}
-              render={({ field, formState: { errors } }) => (
-                <TextField
-                  label="Video Buffer Size (kbps)"
-                  helperText={
-                    errors.transcoding && errors.transcoding.videoBufferSize
-                      ? "Must be a number or 'global'"
-                      : null
-                  }
-                  {...field}
-                />
-              )}
-            />
-          )}
-        </FormControl>
+        </Box>
+        <Divider sx={{ my: 2 }} />
+        <Typography>Transcoding Settings</Typography>
+        <Typography variant="caption">
+          Use these settings to override global ffmpeg settings for this
+          channel.
+        </Typography>
+        <Stack direction="row" gap={2}>
+          <FormControl margin="normal">
+            <InputLabel>Channel Resolution</InputLabel>
+            {ffmpegSettingsLoading ? (
+              <Skeleton>
+                <Input />
+              </Skeleton>
+            ) : (
+              <Controller
+                control={control}
+                name="transcoding.targetResolution"
+                render={() => (
+                  <Select<ResolutionOptionValues>
+                    disabled={
+                      isNil(ffmpegSettings) || !ffmpegSettings.enableTranscoding
+                    }
+                    label="Channel Resolution"
+                    value={targetResString}
+                    onChange={(e) => handleResolutionChange(e)}
+                  >
+                    {allResolutionOptions.map((opt) => (
+                      <MenuItem key={opt.value} value={opt.value}>
+                        {opt.label}
+                      </MenuItem>
+                    ))}
+                  </Select>
+                )}
+              />
+            )}
+          </FormControl>
+          <FormControl margin="normal">
+            {ffmpegSettingsLoading ? (
+              <Skeleton>
+                <Input />
+              </Skeleton>
+            ) : (
+              <Controller
+                control={control}
+                name="transcoding.videoBitrate"
+                disabled={
+                  isNil(ffmpegSettings) || !ffmpegSettings.enableTranscoding
+                }
+                rules={{ pattern: globalOrNumber }}
+                render={({ field, formState: { errors } }) => (
+                  <TextField
+                    label="Video Bitrate (kbps)"
+                    helperText={
+                      errors.transcoding && errors.transcoding.videoBitrate
+                        ? "Must be a number or 'global'"
+                        : null
+                    }
+                    {...field}
+                  />
+                )}
+              />
+            )}
+          </FormControl>
+          <FormControl margin="normal">
+            {ffmpegSettingsLoading ? (
+              <Skeleton>
+                <Input />
+              </Skeleton>
+            ) : (
+              <Controller
+                control={control}
+                name="transcoding.videoBufferSize"
+                disabled={
+                  isNil(ffmpegSettings) || !ffmpegSettings.enableTranscoding
+                }
+                rules={{ pattern: globalOrNumber }}
+                render={({ field, formState: { errors } }) => (
+                  <TextField
+                    label="Video Buffer Size (kbps)"
+                    helperText={
+                      errors.transcoding && errors.transcoding.videoBufferSize
+                        ? "Must be a number or 'global'"
+                        : null
+                    }
+                    {...field}
+                  />
+                )}
+              />
+            )}
+          </FormControl>
+        </Stack>
       </Box>
-    </Box>
+    )
   );
 }
