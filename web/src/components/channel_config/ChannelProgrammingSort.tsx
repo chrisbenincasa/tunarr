407c864402705c285323d5d2a9b4e28ee6a1fce9
Mark D'Avella <markdavella@gmail.com>
2024-04-11 10:38:53 -0400

    fix cyclic shuffle button

--- web/src/components/channel_config/ChannelProgrammingSort.tsx
+++ web/src/components/channel_config/ChannelProgrammingSort.tsx
@@ -202,6 +202,7 @@ export function ChannelProgrammingSort() {
             disableRipple
             onClick={() => {
               setSort('cyclic');
+              cyclicShuffle();
               handleClose();
             }}
           >
