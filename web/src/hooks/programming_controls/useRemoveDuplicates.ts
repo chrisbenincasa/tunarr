5091d360e6386d526c9417526cde916e6dc3f36f
Christian Benincasa <chrisbenincasa@gmail.com>
2024-04-19 13:17:26 -0400

    Remove duplicates should use new externalIds field. Fixes #332

--- web/src/hooks/programming_controls/useRemoveDuplicates.ts
+++ web/src/hooks/programming_controls/useRemoveDuplicates.ts
@@ -1,5 +1,13 @@
-import { ChannelProgram, isFlexProgram, programUniqueId } from '@tunarr/types';
-import { chain, filter, isNull } from 'lodash-es';
+import { createExternalId } from '@tunarr/shared';
+import {
+  ChannelProgram,
+  MultiExternalId,
+  isContentProgram,
+  isCustomProgram,
+  isFlexProgram,
+  isRedirectProgram,
+} from '@tunarr/types';
+import { chain, forEach, reject, some } from 'lodash-es';
 import { setCurrentLineup } from '../../store/channelEditor/actions.ts';
 import useStore from '../../store/index.ts';
 import { materializedProgramListSelector } from '../../store/selectors.ts';
@@ -15,21 +23,39 @@ export const useRemoveDuplicates = () => {
 };
 
 export const removeDuplicatePrograms = (programs: ChannelProgram[]) => {
-  const seenCount = chain(programs)
-    .map(programUniqueId)
-    .reject(isNull)
-    .reduce((acc, id) => ({ ...acc, [id!]: 0 }), {} as Record<string, number>)
-    .value();
-  return filter(programs, (p) => {
-    const uniqueId = programUniqueId(p);
-    if (!uniqueId) {
-      return false;
-    }
+  const seenIds = new Set<string>();
+  const seenRedirects = new Set<string>();
+  const seenCustom = new Set<string>();
 
+  return reject(programs, (p) => {
+    // Removes all flex
     if (isFlexProgram(p)) {
-      return false;
+      return true;
     }
 
-    return seenCount[uniqueId]++ === 0;
+    if (isRedirectProgram(p) || isCustomProgram(p)) {
+      const setToCheck = isRedirectProgram(p) ? seenRedirects : seenCustom;
+      const idToCheck = isRedirectProgram(p) ? p.channel : p.customShowId;
+      const seen = setToCheck.has(idToCheck);
+      if (!seen) {
+        setToCheck.add(idToCheck);
+      }
+      return seen;
+    }
+
+    if (!isContentProgram(p)) {
+      return true;
+    }
+
+    const externalIds = chain(p.externalIds)
+      .filter((id): id is MultiExternalId => id.type === 'multi')
+      .map((id) => createExternalId(id.source, id.sourceId, id.id))
+      .value();
+    const seenAny = some(externalIds, (id) => seenIds.has(id));
+    if (!seenAny) {
+      forEach(externalIds, (id) => seenIds.add(id));
+    }
+
+    return seenAny;
   });
 };
