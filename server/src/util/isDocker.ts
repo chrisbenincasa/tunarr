1319ec2dd8a8532365c9bd87333381e2bbc295d5
Christian Benincasa <chrisbenincasa@gmail.com>
2024-05-14 07:39:59 -0400

    Wrap docker checks in attempt / catch errors

--- server/src/util/isDocker.ts
+++ server/src/util/isDocker.ts
@@ -2,25 +2,27 @@ import { Mutex } from 'async-mutex';
 import fs from 'node:fs/promises';
 import { existsSync, readFileSync } from 'node:fs';
 import { fileExists } from './fsUtil';
-import { isUndefined } from 'lodash-es';
+import { attempt, isError, isUndefined } from 'lodash-es';
 
 const checkerMutex = new Mutex();
 
 let isDockerCached: boolean | undefined;
 
 const hasDockerEnv = () => fileExists('/.dockerenv');
-const hasDockerEnvSync = () => existsSync('/.dockerenv');
+const hasDockerEnvSync = () => attempt(() => existsSync('/.dockerenv'));
 const hasDockerCGroup = async () => {
   const contents = await fs.readFile('/proc/self/cgroup', 'utf-8');
   return contents.includes('docker');
 };
 const hasDockerCGroupSync = () =>
-  readFileSync('/proc/self/cgroup', 'utf-8').includes('docker');
+  attempt(() => readFileSync('/proc/self/cgroup', 'utf-8').includes('docker'));
 
 export function isDockerAsync() {
   return checkerMutex.runExclusive(async () => {
     if (isUndefined(isDockerCached)) {
-      isDockerCached = (await hasDockerEnv()) || (await hasDockerCGroup());
+      isDockerCached =
+        (await hasDockerEnv().catch(() => false)) ||
+        (await hasDockerCGroup().catch(() => false));
     }
     return isDockerCached;
   });
@@ -28,7 +30,12 @@ export function isDockerAsync() {
 
 export function isDocker() {
   if (isUndefined(isDockerCached)) {
-    isDockerCached = hasDockerEnvSync() || hasDockerCGroupSync();
+    const hasEnv = hasDockerEnvSync();
+    isDockerCached = !isError(hasEnv) && hasEnv;
+    if (!isDockerCached) {
+      const hasCgroup = hasDockerCGroupSync();
+      isDockerCached = !isError(hasCgroup) && hasCgroup;
+    }
   }
   return isDockerCached;
 }
