3c1d09f396fa4ed818f6beaef7f12f1368792a44
Christian Benincasa <chrisbenincasa@gmail.com>
2024-06-23 09:34:31 -0400

    See if import.meta.env helps hide dev tools in docker build (#559)

--- web/src/dev/TanStackRouterDevtools.tsx
+++ web/src/dev/TanStackRouterDevtools.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 export const TanStackRouterDevtools =
-  process.env.NODE_ENV === 'production'
+  process.env.NODE_ENV === 'production' || import.meta.env.PROD
     ? () => null // Render nothing in production
     : React.lazy(() =>
         // Lazy load in development
