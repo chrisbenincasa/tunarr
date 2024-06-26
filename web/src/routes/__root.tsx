9b0e4b01abcacb7b60ef0daaeaea3f00d5c28846
Christian Benincasa <chrisbenincasa@gmail.com>
2024-06-26 11:37:52 -0400

    Bring back error page (#581)

--- web/src/routes/__root.tsx
+++ web/src/routes/__root.tsx
@@ -1,7 +1,12 @@
 import { Root } from '@/App';
 import { RouterContext } from '@/types/RouterContext';
-import { Link, createRootRouteWithContext } from '@tanstack/react-router';
+import {
+  Link as RouterLink,
+  createRootRouteWithContext,
+} from '@tanstack/react-router';
 import { TanStackRouterDevtools } from '@/dev/TanStackRouterDevtools';
+import { ErrorPage } from '@/pages/ErrorPage';
+import { Link } from '@mui/material';
 
 export const Route = createRootRouteWithContext<RouterContext>()({
   component: () => (
@@ -13,7 +18,16 @@ export const Route = createRootRouteWithContext<RouterContext>()({
   notFoundComponent: () => (
     <div>
       <p>Not found!</p>
-      <Link to="/">Go home</Link>
+      <Link component={RouterLink} to="/">
+        Go Home
+      </Link>
     </div>
   ),
+  errorComponent: ({ error, reset }) => {
+    return (
+      <Root>
+        <ErrorPage error={error} resetRoute={reset} />
+      </Root>
+    );
+  },
 });
