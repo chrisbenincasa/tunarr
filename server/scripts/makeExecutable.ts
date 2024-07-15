74953e16965c336fe7540ddf63eeb0b3738213fe
Christian Benincasa <chrisbenincasa@gmail.com>
2024-07-15 16:02:20 -0400

    feat: add ARM builds

--- server/scripts/makeExecutable.ts
+++ server/scripts/makeExecutable.ts
@@ -7,10 +7,14 @@ import { hideBin } from 'yargs/helpers';
 import archiver from 'archiver';
 
 const NODE_VERSION = '20.11.1';
-const OSX_TARGET = `macos-x64-${NODE_VERSION}`;
 const LINUX_TARGET = `linux-x64-${NODE_VERSION}`;
 const WINDOWS_TARGET = `windows-x64-${NODE_VERSION}`;
-const ALL_TARGETS = [OSX_TARGET, LINUX_TARGET, WINDOWS_TARGET] as const;
+const LINUX_ARM64V8_TARGET = `linux-arm64v8-${NODE_VERSION}`;
+const ALL_TARGETS = [
+  LINUX_TARGET,
+  WINDOWS_TARGET,
+  LINUX_ARM64V8_TARGET,
+] as const;
 
 const args = await yargs(hideBin(process.argv))
   .scriptName('tunarr-make-exec')
@@ -50,25 +54,22 @@ for (const target of args.target) {
   let binaryName: string;
   let shortArch = target.split('-', 2).join('-');
   switch (target) {
-    case 'macos-x64-20.11.1':
-      binaryName = 'tunarr-macos-x64';
-      break;
     case 'linux-x64-20.11.1':
       binaryName = 'tunarr-linux-x64';
+      break;
+    case 'linux-arm64v8-20.11.1':
+      binaryName = 'tunarr-linux-arm64';
       break;
     case 'windows-x64-20.11.1':
       binaryName = 'tunarr-windows-x64.exe';
       break;
   }
 
-  const sanitizedTargetName =
-    target === 'macos-x64-20.11.1' ? target.replace('macos', 'mac') : target;
-
   await compile({
     input: 'bundle.js',
     name: binaryName,
     cwd: './build',
-    targets: [sanitizedTargetName],
+    targets: [target],
     build: false,
     loglevel: 'verbose',
     bundle: false,
