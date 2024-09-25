ba5a6e328ec51d7dd70ebdf6be6a5c2c7141c762
Christian Benincasa <chrisbenincasa@gmail.com>
2024-09-25 15:44:50 -0400

    fix: only use reconnect parameters when input source is http(s)

--- server/src/ffmpeg/ffmpeg.ts
+++ server/src/ffmpeg/ffmpeg.ts
@@ -469,19 +469,21 @@ export class FFMPEG {
       `+genpts+discardcorrupt+igndts`,
       '-loglevel',
       this.opts.logLevel,
-      '-reconnect',
-      '1',
-      '-reconnect_on_network_error',
-      '1',
-      '-reconnect_streamed',
-      '1',
-      '-multiple_requests',
-      '1',
-      '-chunked_post',
-      '0',
     ];
 
-    let useStillImageTune = false;
+    if (isNonEmptyString(streamSrc) && streamSrc.startsWith('http')) {
+      ffmpegArgs.push(
+        '-reconnect',
+        '1',
+        '-reconnect_on_network_error',
+        '1',
+        '-reconnect_streamed',
+        '1',
+        '-multiple_requests',
+        '1',
+      );
+    }
+
     let artificialBurst = false;
 
     if (!this.audioOnly || isNonEmptyString(streamSrc)) {
