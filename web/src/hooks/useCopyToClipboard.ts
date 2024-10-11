accb0d74f7fdbe61e74e0aca2645ce100fc5b0c2
Christian Benincasa <chrisbenincasa@gmail.com>
2024-10-11 12:29:57 -0400

    fix: use proper m3u links in frontend (#854)

---
+++ web/src/hooks/useCopyToClipboard.ts
@@ -0,0 +1,101 @@
+import { isNonEmptyString } from '@/helpers/util';
+import { useSnackbar } from 'notistack';
+import { useCallback, useEffect, useRef, useState } from 'react';
+import { useIsMounted, useUnmount } from 'usehooks-ts';
+import { useBrowserInfo } from './useBrowserInfo';
+
+export const useCopyToClipboard = () => {
+  const browser = useBrowserInfo();
+  const snackbar = useSnackbar();
+  const [id] = useState(`value${new Date().getTime()}`);
+  const ref = useRef<HTMLDivElement | null>(null);
+
+  const isMounted = useIsMounted()();
+
+  useEffect(() => {
+    if (!window.isSecureContext && isMounted) {
+      if (!ref.current) {
+        const txtarea = document.createElement('div');
+        txtarea.setAttribute(
+          'style',
+          'position:absolute;left:-10000px;top:-10000px',
+        );
+        txtarea.setAttribute('id', id);
+        // txtarea.setAttribute('style', 'display:none');
+        document.body.appendChild(txtarea);
+        ref.current = txtarea;
+      } else if (!ref.current.parentNode) {
+        ref.current.setAttribute('id', id);
+        document.body.appendChild(ref.current);
+      }
+    }
+  }, [id, isMounted]);
+
+  useUnmount(() => {
+    ref.current?.remove();
+    ref.current = null;
+  });
+
+  return useCallback(
+    async (text: string, successText?: string, errorText?: string) => {
+      const popSuccessSnack = () => {
+        if (isNonEmptyString(successText)) {
+          snackbar.enqueueSnackbar(successText, { variant: 'success' });
+        }
+      };
+
+      const popErrorSnack = () => {
+        if (isNonEmptyString(errorText)) {
+          snackbar.enqueueSnackbar(errorText, { variant: 'error' });
+        }
+      };
+
+      if (!window.isSecureContext && ref.current) {
+        ref.current.textContent = text;
+        // HERE BE HACKS
+        const selection = window.getSelection();
+        if (!selection) {
+          popErrorSnack();
+          console.error('Could not get window selection object');
+          return;
+        }
+
+        selection.selectAllChildren(ref.current);
+        const result = document.execCommand('copy');
+        selection.removeAllRanges();
+        if (result) {
+          popSuccessSnack();
+        } else {
+          console.error('Unable to execute copy command');
+          popErrorSnack();
+        }
+
+        return;
+      }
+
+      // Any chromium based browser
+      // This is only supported by chromium: https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/Interact_with_the_clipboard#using_the_clipboard_api
+      try {
+        if (browser.getEngineName() === 'Blink') {
+          console.log('here');
+          const result = await navigator.permissions.query({
+            // @ts-expect-error -- clipboard-write not included in PermissionName enum
+            name: 'clipboard-write',
+          });
+
+          if (result.state === 'granted' || result.state === 'prompt') {
+            await navigator.clipboard.writeText(text);
+          }
+        } else {
+          await navigator.clipboard.writeText(text);
+        }
+
+        popSuccessSnack();
+      } catch (e) {
+        console.error(e, 'Error while attempting to copy to clipboard');
+        popErrorSnack();
+      }
+    },
+    [browser, snackbar],
+  );
+};
