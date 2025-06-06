2d052f751ef9597d076b836b24f2b473037a4839
Christian Benincasa <chrisbenincasa@gmail.com>
2025-06-05 22:27:20 -0400

    fix(streaming): generate correct font file paths for error streams on Windows

--- server/src/ffmpeg/builder/filter/TitleTextFilter.ts
+++ server/src/ffmpeg/builder/filter/TitleTextFilter.ts
@@ -1,5 +1,7 @@
 import type { FrameSize } from '@/ffmpeg/builder/types.js';
 import { serverOptions } from '@/globals.js';
+import path from 'node:path';
+import { isWindows } from '../../../util/index.ts';
 import { FilterOption } from './FilterOption.ts';
 
 export class TitleTextFilter extends FilterOption {
@@ -15,13 +17,17 @@ export class TitleTextFilter extends FilterOption {
     const sz2 = Math.ceil(this.size.height / 33);
     const sz1 = Math.ceil((sz2 * 3) / 2);
     const sz3 = 2 * sz2;
-    return `drawtext=fontfile=${
-      serverOptions().databaseDirectory
-    }/font.ttf:fontsize=${sz1}:fontcolor=white:x=(w-text_w)/2:y=(h-text_h)/2:text='${
+    let fontPath = path
+      .join(serverOptions().databaseDirectory, 'font.ttf')
+      .replaceAll(':', '\\:');
+    if (isWindows()) {
+      // Just use forward slashes on windows. ffmpeg (and windows) can handle it.
+      fontPath = fontPath.replaceAll(path.win32.sep, path.posix.sep);
+    }
+
+    return `drawtext=fontfile=${fontPath}:fontsize=${sz1}:fontcolor=white:x=(w-text_w)/2:y=(h-text_h)/2:text='${
       this.title
-    }',drawtext=fontfile=${
-      serverOptions().databaseDirectory
-    }/font.ttf:fontsize=${sz2}:fontcolor=white:x=(w-text_w)/2:y=(h+text_h+${sz3})/2:text='${
+    }',drawtext=fontfile=${fontPath}:fontsize=${sz2}:fontcolor=white:x=(w-text_w)/2:y=(h+text_h+${sz3})/2:text='${
       this.subtitle ?? ''
     }'`;
   }
