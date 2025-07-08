6438a0db0269a4644298afdbbef6925354f07b0c
Christian Benincasa <chrisbenincasa@gmail.com>
2025-07-08 13:39:32 -0400

    style: various fixes to how the guide page is displayed (#1248)

---
+++ web/src/components/guide/TvGuideGridParent.ts
@@ -0,0 +1,7 @@
+import { Box, styled } from '@mui/material';
+
+export const TvGuideGridParent = styled(Box)({
+  borderStyle: 'solid',
+  borderColor: 'transparent',
+  borderWidth: '1px 0 0 1px',
+});
