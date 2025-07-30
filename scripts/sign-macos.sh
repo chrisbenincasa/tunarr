457cdafe7d1b28f26a9368f7b0382b6642d63aa1
Christian Benincasa <chrisbenincasa@gmail.com>
2025-07-30 09:43:58 -0400

    chore: update signing identity to new cert

--- scripts/sign-macos.sh
+++ scripts/sign-macos.sh
@@ -4,6 +4,6 @@ SCRIPT_FOLDER=$(dirname "${BASH_SOURCE[0]}")
 REPO_ROOT=$(realpath "$SCRIPT_FOLDER/..")
 APP_NAME="$REPO_ROOT/Tunarr.app"
 ENTITLEMENTS="$REPO_ROOT/macos/Tunarr/Tunarr/Tunarr.entitlements"
-SIGNING_IDENTITY="040A9C3F4C8D3D983822CCB65959A6FDB9DACDCD"
+SIGNING_IDENTITY="21F2F5C1AA7E5C352B6EC52AC9BFD58C0AC4B306"
 
 codesign --force --verbose --timestamp --options=runtime --entitlements "$ENTITLEMENTS" --sign "$SIGNING_IDENTITY" --deep "$APP_NAME"
