55aeee5496581a7f6c515060dd844cef33bb17a1
Christian Benincasa <chrisbenincasa@gmail.com>
2025-07-30 12:18:57 -0400

    chore: output all identities before signing (debug)

--- scripts/sign-macos-dmg.sh
+++ scripts/sign-macos-dmg.sh
@@ -8,4 +8,6 @@ DMG_NAME="$REPO_ROOT/Tunarr.dmg"
 ENTITLEMENTS="$REPO_ROOT/macos/Tunarr/Tunarr/Tunarr.entitlements"
 SIGNING_IDENTITY="21F2F5C1AA7E5C352B6EC52AC9BFD58C0AC4B306"
 
+security find-identity -p codesigning -v
+
 codesign --force --timestamp --options=runtime --entitlements "$ENTITLEMENTS" --sign "$SIGNING_IDENTITY" "$DMG_NAME"
--- scripts/sign-macos.sh
+++ scripts/sign-macos.sh
@@ -6,4 +6,6 @@ APP_NAME="$REPO_ROOT/Tunarr.app"
 ENTITLEMENTS="$REPO_ROOT/macos/Tunarr/Tunarr/Tunarr.entitlements"
 SIGNING_IDENTITY="21F2F5C1AA7E5C352B6EC52AC9BFD58C0AC4B306"
 
+security find-identity -p codesigning -v
+
 codesign --force --verbose --timestamp --options=runtime --entitlements "$ENTITLEMENTS" --sign "$SIGNING_IDENTITY" --deep "$APP_NAME"
