4eacb8df1070a31ca10558bd8532f1b09e1cf483
Christian Benincasa <chrisbenincasa@gmail.com>
2025-07-29 22:20:59 -0400

    chore: more potential fixes to macOS build

--- scripts/bundle-macos.sh
+++ scripts/bundle-macos.sh
@@ -12,7 +12,7 @@ fi
 
 pushd "$REPO_ROOT/macos/Tunarr" || exit
 
-xcodebuild build
+xcodebuild build -scheme Tunarr -configuration Release ARCHS="$2"
 cp -R "$REPO_ROOT/macos/Tunarr/build/Release/Tunarr.app" "$APP_NAME"
 
 popd || exit
