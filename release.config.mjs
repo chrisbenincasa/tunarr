101e498ea25da20ada2bfb07ebd4280aff5f46d3
Christian Benincasa <chrisbenincasa@gmail.com>
2025-08-26 13:31:40 -0400

    chore: release config changes

--- release.config.mjs
+++ release.config.mjs
@@ -9,21 +9,23 @@ export default {
     {
       name: 'dev',
       prerelease: true,
+      channel: 'next',
     },
     {
       name: 'media-scanner',
       prerelease: true,
+      channel: 'media-scanner',
     },
   ],
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
-    // [
-    //   '@semantic-release/npm',
-    //   {
-    //     npmPublish: false,
-    //   },
-    // ],
+    [
+      '@semantic-release/npm',
+      {
+        npmPublish: false,
+      },
+    ],
     '@semantic-release/github',
     '@semantic-release/changelog',
   ],
