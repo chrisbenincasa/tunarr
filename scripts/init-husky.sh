6209bf0485d76eb7f2472bba7cf9f35947fe0e82
Christian Benincasa <chrisbenincasa@gmail.com>
2025-09-07 08:37:26 -0400

    chore: fix husky init

--- scripts/init-husky.sh
+++ scripts/init-husky.sh
@@ -3,8 +3,8 @@
 # Check if bunx is installed
 if command -v bun &> /dev/null; then
   # Run the command with bunx
-  bun -e "if (process.env.NODE_ENV !== 'production'){process.exit(1)}" || bunx husky install
+  bun -e "if (process.env.NODE_ENV !== 'production'){process.exit(1)}" || bunx husky
 else
   # Fall back to npx if bunx is not available
-  node -e "if (process.env.NODE_ENV !== 'production'){process.exit(1)}" || husky install
+  node -e "if (process.env.NODE_ENV !== 'production'){process.exit(1)}" || npx husky
 fi
