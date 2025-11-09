7f54e90856e434c08cf94c147c52298b9b57a8a5
Christian Benincasa <chrisbenincasa@gmail.com>
2025-11-09 12:23:04 -0500

    chore: remove unused code

--- server/src/util/logging/basicPrettyTransport.ts
+++
@@ -1,9 +0,0 @@
-import { once } from 'node:events';
-import { createWriteStream } from 'node:fs';
-
-export default async (options) => {
-  console.log(options);
-  const stream = createWriteStream('test.log');
-  await once(stream, 'open');
-  return stream;
-};
