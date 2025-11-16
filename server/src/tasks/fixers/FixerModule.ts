f6cf17f27000500b5b71a67e99318696c35436c2
Christian Benincasa <chrisbenincasa@gmail.com>
2025-11-15 22:16:22 -0500

    fix: remove BackfillProgramGroupings fixer

--- server/src/tasks/fixers/FixerModule.ts
+++ server/src/tasks/fixers/FixerModule.ts
@@ -1,6 +1,5 @@
 import { EnsureTranscodeConfigIds } from '@/tasks/fixers/EnsureTranscodeConfigIds.js';
 import { AddPlexServerIdsFixer } from '@/tasks/fixers/addPlexServerIds.js';
-import { BackfillProgramGroupings } from '@/tasks/fixers/backfillProgramGroupings.js';
 import type Fixer from '@/tasks/fixers/fixer.js';
 import { MissingSeasonNumbersFixer } from '@/tasks/fixers/missingSeasonNumbersFixer.js';
 import { KEYS } from '@/types/inject.js';
@@ -12,7 +11,6 @@ const FixerModule = new ContainerModule((bind) => {
   bind<Fixer>(KEYS.Fixer).to(BackfillProgramExternalIds);
   bind<Fixer>(KEYS.Fixer).to(EnsureTranscodeConfigIds);
   bind<Fixer>(KEYS.Fixer).to(AddPlexServerIdsFixer);
-  bind<Fixer>(KEYS.Fixer).to(BackfillProgramGroupings);
   bind<Fixer>(KEYS.Fixer).to(MissingSeasonNumbersFixer);
   bind<Fixer>(KEYS.Fixer).to(BackfillMediaSourceIdFixer);
 });
