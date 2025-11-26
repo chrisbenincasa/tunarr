f8a2a2464ad889d763488a5580d780e9be18da6e
Christian Benincasa <chrisbenincasa@gmail.com>
2025-11-26 09:53:15 -0500

    fix: properly detect non-docker/podman container environments
    Fixes: #1485

--- server/src/util/containerUtil.ts
+++ server/src/util/containerUtil.ts
@@ -2,20 +2,45 @@ import { Mutex } from 'async-mutex';
 import { attempt, isError, isUndefined } from 'lodash-es';
 import { existsSync, readFileSync } from 'node:fs';
 import fs from 'node:fs/promises';
+import type { Maybe } from '../types/util.ts';
 import { fileExists } from './fsUtil.ts';
 
 const checkerMutex = new Mutex();
+const cGroupReadMutex = new Mutex();
 
 let isDockerCached: boolean | undefined;
+let cachedCgroupContents: Maybe<string>;
+
+const readCgroupAsync = async () => {
+  return cGroupReadMutex.runExclusive(async () => {
+    if (cachedCgroupContents) {
+      return cachedCgroupContents;
+    }
+    cachedCgroupContents = await fs.readFile('/proc/self/cgroup', 'utf-8');
+    return cachedCgroupContents;
+  });
+};
+
+const readCgroupSync = () => {
+  if (cachedCgroupContents) {
+    return cachedCgroupContents;
+  }
+  cachedCgroupContents = readFileSync('/proc/self/cgroup', 'utf-8');
+  return cachedCgroupContents;
+};
 
 const hasDockerEnv = () => fileExists('/.dockerenv');
 const hasDockerEnvSync = () => attempt(() => existsSync('/.dockerenv'));
 const hasDockerCGroup = async () => {
-  const contents = await fs.readFile('/proc/self/cgroup', 'utf-8');
-  return contents.includes('docker');
+  return (await readCgroupAsync()).includes('docker');
 };
-const hasDockerCGroupSync = () =>
-  attempt(() => readFileSync('/proc/self/cgroup', 'utf-8').includes('docker'));
+const hasDockerCGroupSync = () => {
+  try {
+    return readCgroupSync().includes('docker');
+  } catch {
+    return false;
+  }
+};
 
 export function isDockerAsync() {
   return checkerMutex.runExclusive(async () => {
@@ -44,6 +69,14 @@ export function isPodman() {
   return process.env['CONTAINER'] === 'podman';
 }
 
+export function isOtherwiseContainerized() {
+  try {
+    return readCgroupSync().toLowerCase().includes('containerd');
+  } catch {
+    return false;
+  }
+}
+
 export function isRunningInContainer() {
-  return isDocker() || isPodman();
+  return isDocker() || isPodman() || isOtherwiseContainerized();
 }
