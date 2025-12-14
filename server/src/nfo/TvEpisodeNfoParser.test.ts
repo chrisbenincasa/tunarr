288466f42cf9765658f2dc3e1ca23a95206a4ea3
Justin <justinforlenza@users.noreply.github.com>
2025-12-14 23:21:08 +0000

    test: changes tests for Movie and Episode parsers to check directors/credits

--- server/src/nfo/TvEpisodeNfoParser.test.ts
+++ server/src/nfo/TvEpisodeNfoParser.test.ts
@@ -4,29 +4,47 @@ import { TvEpisodeNfoParser } from './TvEpisodeNfoParser.ts';
 describe('TvEpisodeNfoParser', () => {
   test('parses basic tv episode file', async () => {
     const contents = (await readTestFile('episode.nfo')).toString('utf-8');
-    const output = await new TvEpisodeNfoParser().parse(contents);
-    console.log(output.get().episodedetails);
+    const result = await new TvEpisodeNfoParser().parse(contents);
+    expect(result.isSuccess()).toBe(true);
+
+    const output = result.get();
+
+    expect(output.episodedetails[0].credits).length(1)
+
+    console.log(output.episodedetails);
   });
 
   test('parses basic tv episode file 2', async () => {
     const contents = (await readTestFile('episode2.nfo')).toString('utf-8');
-    const output = await new TvEpisodeNfoParser().parse(contents);
-    console.log(output.get().episodedetails);
+    const result = await new TvEpisodeNfoParser().parse(contents);
+    expect(result.isSuccess()).toBe(true);
+
+    const output = result.get();
+
+    console.log(output.episodedetails);
   });
 
   test('parses multi tv episode file', async () => {
     const contents = (await readTestFile('multi-episode.nfo')).toString(
       'utf-8',
     );
-    const output = await new TvEpisodeNfoParser().parse(contents);
-    console.log(output.get().episodedetails);
+    const result = await new TvEpisodeNfoParser().parse(contents);
+    expect(result.isSuccess()).toBe(true);
+
+    const output = result.get();
+
+    console.log(output.episodedetails);
   });
 
   test('parses tv episode file with no thumbs', async () => {
     const contents = (await readTestFile('episode_no_thumbs.nfo')).toString(
       'utf-8',
     );
-    const output = await new TvEpisodeNfoParser().parse(contents);
-    console.log(output.get().episodedetails);
+    const result = await new TvEpisodeNfoParser().parse(contents);
+    expect(result.isSuccess()).toBe(true);
+
+    const output = result.get();
+
+    console.log(output.episodedetails);
   });
 });
