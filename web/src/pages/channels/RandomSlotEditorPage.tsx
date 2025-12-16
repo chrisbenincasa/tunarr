4dac5a046efb7824f7100dab33d130a00fcd2b12
Christian Benincasa <chrisbenincasa@gmail.com>
2025-12-16 15:11:03 -0500

    chore: fix build break

--- web/src/pages/channels/RandomSlotEditorPage.tsx
+++ web/src/pages/channels/RandomSlotEditorPage.tsx
@@ -91,12 +91,8 @@ export default function RandomSlotEditorPage() {
         : defaultRandomSlotSchedule,
   });
 
-  const {
-    control,
-    getValues,
-    formState: { isValid, isDirty },
-    reset,
-  } = randomSlotForm;
+  const { control, getValues, formState, reset } = randomSlotForm;
+  const { isDirty } = formState;
 
   const slotArray = useFieldArray({
     control,
@@ -253,11 +249,7 @@ export default function RandomSlotEditorPage() {
             Reset Options
           </Button>
         )}
-        <Button
-          variant="contained"
-          // disabled={!isValid}
-          onClick={() => onSave()}
-        >
+        <Button variant="contained" onClick={() => onSave()}>
           Save
         </Button>
       </Box>
