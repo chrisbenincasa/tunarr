5bfcf6b0181cb90241ff575be3fed8dc23602570
Christian Benincasa <chrisbenincasa@gmail.com>
2025-12-17 11:24:22 -0500

    fix: fix building external thumb URLs for unsynced libraries

--- web/src/hooks/useThumbnailUrl.ts
+++ web/src/hooks/useThumbnailUrl.ts
@@ -4,13 +4,20 @@ import type { Person, ProgramOrFolder } from '@tunarr/types';
 import { isStructuralItemType, tag } from '@tunarr/types';
 import type { MediaArtworkType } from '@tunarr/types/schemas';
 import { groupBy, isEmpty } from 'lodash-es';
-import { useCallback } from 'react';
+import { useCallback, useMemo } from 'react';
 import type { MarkOptional } from 'ts-essentials';
 import type z from 'zod';
 import { useSettings } from '../store/settings/selectors.ts';
 
 export const useGetArtworkUrl = () => {
   const settings = useSettings();
+  const actualBackendUri = useMemo(
+    () =>
+      isEmpty(settings.backendUri)
+        ? window.location.origin
+        : settings.backendUri,
+    [settings.backendUri],
+  );
   return useCallback(
     (
       item: ProgramOrFolder,
@@ -31,11 +38,11 @@ export const useGetArtworkUrl = () => {
             isNonEmptyString(art.id),
           );
           if (persistedArt || item.sourceType === 'local') {
-            return `${settings.backendUri}/api/programs/${item.uuid}/artwork/${type}`;
+            return `${actualBackendUri}/api/programs/${item.uuid}/artwork/${type}`;
           }
 
           // TODO: Just return the right URLs in the artwork item itself!
-          const url = new URL(`${settings.backendUri}/api/metadata/external`);
+          const url = new URL(`/api/metadata/external`, actualBackendUri);
           url.searchParams.append('asset', 'image');
           url.searchParams.append('imageType', 'poster');
           url.searchParams.append(
@@ -57,7 +64,7 @@ export const useGetArtworkUrl = () => {
 
       return null;
     },
-    [settings.backendUri],
+    [actualBackendUri],
   );
 };
 
