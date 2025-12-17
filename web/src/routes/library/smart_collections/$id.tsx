528790b13ccc1d75c8991510ececa878cfe9649e
Christian Benincasa <chrisbenincasa@gmail.com>
2025-12-17 11:16:35 -0500

    style: add smart collection page (#1542)
    Closes #1526

--- web/src/routes/library/smart_collections/$id.tsx
+++ web/src/routes/library/smart_collections/$id.tsx
@@ -1,9 +1,36 @@
+import { Stack, Typography } from '@mui/material';
+import { useSuspenseQuery } from '@tanstack/react-query';
 import { createFileRoute } from '@tanstack/react-router';
+import { LibrarySearch } from '../../../components/library/LibrarySearch.tsx';
+import { getApiSmartCollectionsByIdOptions } from '../../../generated/@tanstack/react-query.gen.ts';
 
 export const Route = createFileRoute('/library/smart_collections/$id')({
+  loader: async ({ context, params }) => {
+    await context.queryClient.ensureQueryData(
+      getApiSmartCollectionsByIdOptions({
+        path: {
+          id: params.id,
+        },
+      }),
+    );
+  },
   component: RouteComponent,
 });
 
 function RouteComponent() {
-  return <div>Hello "/smart_collections/$id"!</div>;
+  const { id } = Route.useParams();
+  const { data: smartCollection } = useSuspenseQuery(
+    getApiSmartCollectionsByIdOptions({ path: { id } }),
+  );
+  return (
+    <Stack gap={2}>
+      <Typography variant="h4">
+        Smart Collection: {smartCollection.name}
+      </Typography>
+      <LibrarySearch
+        disableProgramSelection
+        initialSearchQuery={smartCollection.query}
+      />
+    </Stack>
+  );
 }
