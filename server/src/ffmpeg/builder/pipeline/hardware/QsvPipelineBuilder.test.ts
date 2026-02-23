c49774e0f72c10dffca267754f97d7b27588b5b9
Christian Benincasa <chrisbenincasa@gmail.com>
2026-02-23 13:53:40 -0500

    test: add snapshot test baseline for current pipelines

--- server/src/ffmpeg/builder/pipeline/hardware/QsvPipelineBuilder.test.ts
+++ server/src/ffmpeg/builder/pipeline/hardware/QsvPipelineBuilder.test.ts
@@ -24,42 +24,108 @@ import { FrameState } from '../../state/FrameState.ts';
 import { FrameSize } from '../../types.ts';
 import { QsvPipelineBuilder } from './QsvPipelineBuilder.ts';
 
+// Shared test fixtures
+
+const ffmpegVersion = {
+  versionString: 'n7.0.2-15-g0458a86656-20240904',
+  majorVersion: 7,
+  minorVersion: 0,
+  patchVersion: 2,
+  isUnknown: false,
+} as const;
+
+function makeVaapiCapabilities(
+  profiles: VaapiProfileEntrypoint[] = [],
+): VaapiHardwareCapabilities {
+  return new VaapiHardwareCapabilities(profiles);
+}
+
+function makeH264DecodeEncodeCapabilities(): VaapiHardwareCapabilities {
+  return new VaapiHardwareCapabilities([
+    new VaapiProfileEntrypoint(VaapiProfiles.H264Main, VaapiEntrypoint.Decode),
+    new VaapiProfileEntrypoint(VaapiProfiles.H264Main, VaapiEntrypoint.Encode),
+  ]);
+}
+
+function makeEmptyBinaryCapabilities(): FfmpegCapabilities {
+  return new FfmpegCapabilities(new Set(), new Map(), new Set());
+}
+
+function makeH264VideoInputNonSquare(): VideoInputSource {
+  return VideoInputSource.withStream(
+    new FileStreamSource('/path/to/video.mkv'),
+    VideoStream.create({
+      codec: 'h264',
+      displayAspectRatio: '16:9',
+      frameSize: FrameSize.withDimensions(1920, 900),
+      index: 0,
+      pixelFormat: new PixelFormatYuv420P(),
+      providedSampleAspectRatio: null,
+    }),
+  );
+}
+
+function makeH264ProfileVideoInput(): VideoInputSource {
+  return VideoInputSource.withStream(
+    new FileStreamSource('/path/to/video.mkv'),
+    VideoStream.create({
+      codec: 'h264',
+      profile: 'main',
+      displayAspectRatio: '16:9',
+      frameSize: FrameSize.withDimensions(1920, 900),
+      index: 0,
+      pixelFormat: new PixelFormatYuv420P(),
+      providedSampleAspectRatio: null,
+    }),
+  );
+}
+
+function makeWatermark(duration = 5): WatermarkInputSource {
+  return new WatermarkInputSource(
+    new FileStreamSource('/path/to/watermark.jpg'),
+    StillImageStream.create({
+      frameSize: FrameSize.withDimensions(800, 600),
+      index: 0,
+    }),
+    {
+      duration,
+      enabled: true,
+      horizontalMargin: 5,
+      opacity: 100,
+      position: 'bottom-right',
+      verticalMargin: 5,
+      width: 10,
+    },
+  );
+}
+
+function makePgsSubtitles(): SubtitlesInputSource {
+  return new SubtitlesInputSource(
+    new FileStreamSource('/path/to/video.mkv'),
+    [new EmbeddedSubtitleStream('pgs', 5, SubtitleMethods.Burn)],
+    SubtitleMethods.Burn,
+  );
+}
+
+function makeDesiredFrameState(
+  video: VideoInputSource,
+  overrides?: Partial<ConstructorParameters<typeof FrameState>[0]>,
+): FrameState {
+  return new FrameState({
+    isAnamorphic: false,
+    scaledSize: video.streams[0].squarePixelFrameSize(FrameSize.FHD),
+    paddedSize: FrameSize.FHD,
+    pixelFormat: new PixelFormatYuv420P(),
+    ...overrides,
+  });
+}
+
 describe('QsvPipelineBuilder', () => {
   test('should work', () => {
-    const capabilities = new VaapiHardwareCapabilities([]);
-    const binaryCapabilities = new FfmpegCapabilities(
-      new Set(),
-      new Map(),
-      new Set(),
-    );
-    const video = VideoInputSource.withStream(
-      new FileStreamSource('/path/to/video.mkv'),
-      VideoStream.create({
-        codec: 'h264',
-        displayAspectRatio: '16:9',
-        frameSize: FrameSize.withDimensions(1920, 900),
-        index: 0,
-        pixelFormat: new PixelFormatYuv420P(),
-        providedSampleAspectRatio: null,
-      }),
-    );
-
-    const watermark = new WatermarkInputSource(
-      new FileStreamSource('/path/to/watermark.jpg'),
-      StillImageStream.create({
-        frameSize: FrameSize.withDimensions(800, 600),
-        index: 0,
-      }),
-      {
-        duration: 5,
-        enabled: true,
-        horizontalMargin: 5,
-        opacity: 100,
-        position: 'bottom-right',
-        verticalMargin: 5,
-        width: 10,
-      },
-    );
+    const capabilities = makeVaapiCapabilities();
+    const binaryCapabilities = makeEmptyBinaryCapabilities();
+    const video = makeH264VideoInputNonSquare();
+    const watermark = makeWatermark();
 
     const builder = new QsvPipelineBuilder(
       capabilities,
@@ -68,23 +134,10 @@ describe('QsvPipelineBuilder', () => {
       null,
       null,
       watermark,
-      new SubtitlesInputSource(
-        new FileStreamSource('/path/to/video.mkv'),
-        [new EmbeddedSubtitleStream('pgs', 5, SubtitleMethods.Burn)],
-        SubtitleMethods.Burn,
-      ),
+      makePgsSubtitles(),
     );
 
-    const state = FfmpegState.create({
-      version: {
-        versionString: 'n7.0.2-15-g0458a86656-20240904',
-        majorVersion: 7,
-        minorVersion: 0,
-        patchVersion: 2,
-        isUnknown: false,
-      },
-      // start: +dayjs.duration(0),
-    });
+    const state = FfmpegState.create({ version: ffmpegVersion });
 
     const out = builder.build(
       state,
@@ -98,53 +151,61 @@ describe('QsvPipelineBuilder', () => {
       DefaultPipelineOptions,
     );
 
-    console.log(out.getCommandArgs().join(' '));
+    expect(out.getCommandArgs()).toMatchInlineSnapshot(`
+      [
+        "-nostdin",
+        "-hide_banner",
+        "-nostats",
+        "-loglevel",
+        "error",
+        "-fflags",
+        "+genpts+discardcorrupt+igndts",
+        "-init_hw_device",
+        "qsv=hw:hw,child_device_type=vaapi",
+        "-filter_hw_device",
+        "hw",
+        "-c:v",
+        "h264",
+        "-readrate",
+        "1",
+        "-i",
+        "/path/to/video.mkv",
+        "-i",
+        "/path/to/watermark.jpg",
+        "-filter_complex",
+        "[0:0]setpts=PTS-STARTPTS,fps=24,pad=1920:1080:-1:-1:color=black[v];[1:0]scale=192:-1,format=yuva420p[wm];[v][wm]overlay=x=W-w-96:y=H-h-54:format=0:enable='between(t,0,5)'[vwm]",
+        "-map",
+        "[vwm]",
+        "-map",
+        "0:a",
+        "-muxdelay",
+        "0",
+        "-muxpreload",
+        "0",
+        "-flags",
+        "cgop",
+        "-movflags",
+        "+faststart",
+        "-r",
+        "24",
+        "-fps_mode",
+        "cfr",
+        "-sc_threshold",
+        "0",
+        "-c:a",
+        "copy",
+        "-f",
+        "mpegts",
+        "pipe:1",
+      ]
+    `);
   });
 
   test('should work, decoding disabled', () => {
-    const capabilities = new VaapiHardwareCapabilities([
-      new VaapiProfileEntrypoint(
-        VaapiProfiles.H264Main,
-        VaapiEntrypoint.Decode,
-      ),
-      new VaapiProfileEntrypoint(
-        VaapiProfiles.H264Main,
-        VaapiEntrypoint.Encode,
-      ),
-    ]);
-    const binaryCapabilities = new FfmpegCapabilities(
-      new Set(),
-      new Map(),
-      new Set(),
-    );
-    const video = VideoInputSource.withStream(
-      new FileStreamSource('/path/to/video.mkv'),
-      VideoStream.create({
-        codec: 'h264',
-        displayAspectRatio: '16:9',
-        frameSize: FrameSize.withDimensions(1920, 900),
-        index: 0,
-        pixelFormat: new PixelFormatYuv420P(),
-        providedSampleAspectRatio: null,
-      }),
-    );
-
-    const watermark = new WatermarkInputSource(
-      new FileStreamSource('/path/to/watermark.jpg'),
-      StillImageStream.create({
-        frameSize: FrameSize.withDimensions(800, 600),
-        index: 0,
-      }),
-      {
-        duration: 5,
-        enabled: true,
-        horizontalMargin: 5,
-        opacity: 100,
-        position: 'bottom-right',
-        verticalMargin: 5,
-        width: 10,
-      },
-    );
+    const capabilities = makeH264DecodeEncodeCapabilities();
+    const binaryCapabilities = makeEmptyBinaryCapabilities();
+    const video = makeH264VideoInputNonSquare();
+    const watermark = makeWatermark();
 
     const builder = new QsvPipelineBuilder(
       capabilities,
@@ -153,84 +214,72 @@ describe('QsvPipelineBuilder', () => {
       null,
       null,
       watermark,
-      new SubtitlesInputSource(
-        new FileStreamSource('/path/to/video.mkv'),
-        [new EmbeddedSubtitleStream('pgs', 5, SubtitleMethods.Burn)],
-        SubtitleMethods.Burn,
-      ),
+      makePgsSubtitles(),
     );
 
-    const state = FfmpegState.create({
-      version: {
-        versionString: 'n7.0.2-15-g0458a86656-20240904',
-        majorVersion: 7,
-        minorVersion: 0,
-        patchVersion: 2,
-        isUnknown: false,
-      },
-      // start: +dayjs.duration(0),
-    });
+    const state = FfmpegState.create({ version: ffmpegVersion });
 
     const out = builder.build(
       state,
-      new FrameState({
-        isAnamorphic: false,
-        scaledSize: video.streams[0].squarePixelFrameSize(FrameSize.FHD),
-        paddedSize: FrameSize.FHD,
-        pixelFormat: new PixelFormatYuv420P(),
-        videoFormat: 'h264',
-      }),
+      makeDesiredFrameState(video, { videoFormat: 'h264' }),
       { ...DefaultPipelineOptions, disableHardwareDecoding: true },
     );
 
-    console.log(out.getCommandArgs().join(' '));
+    expect(out.getCommandArgs()).toMatchInlineSnapshot(`
+      [
+        "-nostdin",
+        "-hide_banner",
+        "-nostats",
+        "-loglevel",
+        "error",
+        "-fflags",
+        "+genpts+discardcorrupt+igndts",
+        "-init_hw_device",
+        "qsv=hw:hw,child_device_type=vaapi",
+        "-filter_hw_device",
+        "hw",
+        "-c:v",
+        "h264",
+        "-readrate",
+        "1",
+        "-i",
+        "/path/to/video.mkv",
+        "-i",
+        "/path/to/watermark.jpg",
+        "-filter_complex",
+        "[0:0]setpts=PTS-STARTPTS,fps=24,pad=1920:1080:-1:-1:color=black[v];[1:0]scale=192:-1,format=yuva420p[wm];[v][wm]overlay=x=W-w-96:y=H-h-54:format=0:enable='between(t,0,5)'[vwm]",
+        "-map",
+        "[vwm]",
+        "-map",
+        "0:a",
+        "-muxdelay",
+        "0",
+        "-muxpreload",
+        "0",
+        "-flags",
+        "cgop",
+        "-movflags",
+        "+faststart",
+        "-c:v",
+        "h264_qsv",
+        "-low_power",
+        "0",
+        "-look_ahead",
+        "0",
+        "-c:a",
+        "copy",
+        "-f",
+        "mpegts",
+        "pipe:1",
+      ]
+    `);
   });
 
   test('should work, encoding disabled', () => {
-    const capabilities = new VaapiHardwareCapabilities([
-      new VaapiProfileEntrypoint(
-        VaapiProfiles.H264Main,
-        VaapiEntrypoint.Decode,
-      ),
-      new VaapiProfileEntrypoint(
-        VaapiProfiles.H264Main,
-        VaapiEntrypoint.Encode,
-      ),
-    ]);
-    const binaryCapabilities = new FfmpegCapabilities(
-      new Set(),
-      new Map(),
-      new Set(),
-    );
-    const video = VideoInputSource.withStream(
-      new FileStreamSource('/path/to/video.mkv'),
-      VideoStream.create({
-        codec: 'h264',
-        profile: 'main',
-        displayAspectRatio: '16:9',
-        frameSize: FrameSize.withDimensions(1920, 900),
-        index: 0,
-        pixelFormat: new PixelFormatYuv420P(),
-        providedSampleAspectRatio: null,
-      }),
-    );
-
-    const watermark = new WatermarkInputSource(
-      new FileStreamSource('/path/to/watermark.jpg'),
-      StillImageStream.create({
-        frameSize: FrameSize.withDimensions(800, 600),
-        index: 0,
-      }),
-      {
-        duration: 0,
-        enabled: true,
-        horizontalMargin: 5,
-        opacity: 100,
-        position: 'bottom-right',
-        verticalMargin: 5,
-        width: 10,
-      },
-    );
+    const capabilities = makeH264DecodeEncodeCapabilities();
+    const binaryCapabilities = makeEmptyBinaryCapabilities();
+    const video = makeH264ProfileVideoInput();
+    const watermark = makeWatermark(0);
 
     const builder = new QsvPipelineBuilder(
       capabilities,
@@ -242,77 +291,71 @@ describe('QsvPipelineBuilder', () => {
       null,
     );
 
-    const state = FfmpegState.create({
-      version: {
-        versionString: 'n7.0.2-15-g0458a86656-20240904',
-        majorVersion: 7,
-        minorVersion: 0,
-        patchVersion: 2,
-        isUnknown: false,
-      },
-      // start: +dayjs.duration(0),
-    });
+    const state = FfmpegState.create({ version: ffmpegVersion });
 
     const out = builder.build(
       state,
-      new FrameState({
-        isAnamorphic: false,
-        scaledSize: video.streams[0].squarePixelFrameSize(FrameSize.FHD),
-        paddedSize: FrameSize.FHD,
-        pixelFormat: new PixelFormatYuv420P(),
-        videoFormat: 'h264',
-      }),
+      makeDesiredFrameState(video, { videoFormat: 'h264' }),
       { ...DefaultPipelineOptions, disableHardwareEncoding: true },
     );
 
-    console.log(out.getCommandArgs().join(' '));
+    expect(out.getCommandArgs()).toMatchInlineSnapshot(`
+      [
+        "-threads",
+        "1",
+        "-nostdin",
+        "-hide_banner",
+        "-nostats",
+        "-loglevel",
+        "error",
+        "-fflags",
+        "+genpts+discardcorrupt+igndts",
+        "-hwaccel",
+        "qsv",
+        "-hwaccel_output_format",
+        "qsv",
+        "-init_hw_device",
+        "qsv=hw:hw,child_device_type=vaapi",
+        "-filter_hw_device",
+        "hw",
+        "-c:v",
+        "h264_qsv",
+        "-readrate",
+        "1",
+        "-i",
+        "/path/to/video.mkv",
+        "-i",
+        "/path/to/watermark.jpg",
+        "-filter_complex",
+        "[0:0]setpts=PTS-STARTPTS,fps=24,hwdownload,format=nv12,pad=1920:1080:-1:-1:color=black[v];[1:0]scale=192:-1,format=yuva420p[wm];[v][wm]overlay=x=W-w-96:y=H-h-54:format=0[vwm]",
+        "-map",
+        "[vwm]",
+        "-map",
+        "0:a",
+        "-muxdelay",
+        "0",
+        "-muxpreload",
+        "0",
+        "-flags",
+        "cgop",
+        "-movflags",
+        "+faststart",
+        "-sc_threshold",
+        "0",
+        "-c:a",
+        "copy",
+        "-f",
+        "mpegts",
+        "pipe:1",
+      ]
+    `);
   });
 
   test('should work, filters disabled', () => {
-    const capabilities = new VaapiHardwareCapabilities([
-      new VaapiProfileEntrypoint(
-        VaapiProfiles.H264Main,
-        VaapiEntrypoint.Decode,
-      ),
-      new VaapiProfileEntrypoint(
-        VaapiProfiles.H264Main,
-        VaapiEntrypoint.Encode,
-      ),
-    ]);
-    const binaryCapabilities = new FfmpegCapabilities(
-      new Set(),
-      new Map(),
-      new Set(),
-    );
-    const video = VideoInputSource.withStream(
-      new FileStreamSource('/path/to/video.mkv'),
-      VideoStream.create({
-        codec: 'h264',
-        profile: 'main',
-        displayAspectRatio: '16:9',
-        frameSize: FrameSize.withDimensions(1920, 900),
-        index: 0,
-        pixelFormat: new PixelFormatYuv420P(),
-        providedSampleAspectRatio: null,
-      }),
-    );
-
-    const watermark = new WatermarkInputSource(
-      new FileStreamSource('/path/to/watermark.jpg'),
-      StillImageStream.create({
-        frameSize: FrameSize.withDimensions(800, 600),
-        index: 0,
-      }),
-      {
-        duration: 5,
-        enabled: true,
-        horizontalMargin: 5,
-        opacity: 100,
-        position: 'bottom-right',
-        verticalMargin: 5,
-        width: 10,
-      },
-    );
+    const capabilities = makeH264DecodeEncodeCapabilities();
+    const binaryCapabilities = makeEmptyBinaryCapabilities();
+    const video = makeH264ProfileVideoInput();
+    const watermark = makeWatermark();
 
     const builder = new QsvPipelineBuilder(
       capabilities,
@@ -321,36 +364,70 @@ describe('QsvPipelineBuilder', () => {
       null,
       null,
       watermark,
-      new SubtitlesInputSource(
-        new FileStreamSource('/path/to/video.mkv'),
-        [new EmbeddedSubtitleStream('pgs', 5, SubtitleMethods.Burn)],
-        SubtitleMethods.Burn,
-      ),
+      makePgsSubtitles(),
     );
 
-    const state = FfmpegState.create({
-      version: {
-        versionString: 'n7.0.2-15-g0458a86656-20240904',
-        majorVersion: 7,
-        minorVersion: 0,
-        patchVersion: 2,
-        isUnknown: false,
-      },
-      // start: +dayjs.duration(0),
-    });
+    const state = FfmpegState.create({ version: ffmpegVersion });
 
     const out = builder.build(
       state,
-      new FrameState({
-        isAnamorphic: false,
-        scaledSize: video.streams[0].squarePixelFrameSize(FrameSize.FHD),
-        paddedSize: FrameSize.FHD,
-        pixelFormat: new PixelFormatYuv420P(),
-        videoFormat: 'h264',
-      }),
+      makeDesiredFrameState(video, { videoFormat: 'h264' }),
       { ...DefaultPipelineOptions, disableHardwareFilters: true },
     );
 
-    console.log(out.getCommandArgs().join(' '));
+    expect(out.getCommandArgs()).toMatchInlineSnapshot(`
+      [
+        "-threads",
+        "1",
+        "-nostdin",
+        "-hide_banner",
+        "-nostats",
+        "-loglevel",
+        "error",
+        "-fflags",
+        "+genpts+discardcorrupt+igndts",
+        "-hwaccel",
+        "qsv",
+        "-hwaccel_output_format",
+        "qsv",
+        "-init_hw_device",
+        "qsv=hw:hw,child_device_type=vaapi",
+        "-filter_hw_device",
+        "hw",
+        "-c:v",
+        "h264_qsv",
+        "-readrate",
+        "1",
+        "-i",
+        "/path/to/video.mkv",
+        "-i",
+        "/path/to/watermark.jpg",
+        "-filter_complex",
+        "[0:0]setpts=PTS-STARTPTS,fps=24,hwdownload,format=nv12,pad=1920:1080:-1:-1:color=black[v];[1:0]scale=192:-1,format=yuva420p[wm];[v][wm]overlay=x=W-w-96:y=H-h-54:format=0:enable='between(t,0,5)'[vwm]",
+        "-map",
+        "[vwm]",
+        "-map",
+        "0:a",
+        "-muxdelay",
+        "0",
+        "-muxpreload",
+        "0",
+        "-flags",
+        "cgop",
+        "-movflags",
+        "+faststart",
+        "-c:v",
+        "h264_qsv",
+        "-low_power",
+        "0",
+        "-look_ahead",
+        "0",
+        "-c:a",
+        "copy",
+        "-f",
+        "mpegts",
+        "pipe:1",
+      ]
+    `);
   });
 });
