c8f52c07a73db69489d26d7f54c95f19d155fbca
Corey Vaillancourt <coreyjv@gmail.com>
2026-03-24 19:18:05 -0400

    fix: improve user experience of bottom navigation
    This change improves bottom navigation in the following ways:
    - Adds labels and proper ARIA handling
    - Highlights active item (including related subpages)
    - Adds badge support from System page
    - Fixes to stay above page content.

--- web/src/components/BottomNavBar.tsx
+++ web/src/components/BottomNavBar.tsx
@@ -1,30 +1,60 @@
-import { AppBar, IconButton, Toolbar } from '@mui/material';
-import { Link } from '@tanstack/react-router';
-import React from 'react';
+import {
+  Badge,
+  BottomNavigation,
+  BottomNavigationAction,
+  Paper,
+} from '@mui/material';
+import { Link, useRouterState } from '@tanstack/react-router';
 import { useNavItems } from '../hooks/useNavItems.tsx';
+
+const getBaseSection = (path: string) => '/' + path.split('/')[1];
 
 export const BottomNavBar = () => {
   const navItems = useNavItems();
+  const pathname = useRouterState({ select: (s) => s.location.pathname });
+
+  const currentSection = getBaseSection(pathname);
+  const activeItem = navItems
+    .filter((item) => !item.hidden)
+    .find((item) => getBaseSection(item.path) === currentSection);
 
   return (
-    <AppBar position="fixed" sx={{ top: 'auto', bottom: 0 }}>
-      <Toolbar sx={{ display: 'flex', justifyContent: 'space-around' }}>
+    <Paper
+      sx={{
+        position: 'fixed',
+        bottom: 0,
+        left: 0,
+        right: 0,
+        zIndex: (theme) => theme.zIndex.appBar,
+      }}
+      elevation={3}
+    >
+      <BottomNavigation value={activeItem?.path ?? false} showLabels>
         {navItems
           .filter((item) => !item.hidden)
           .map((item) => (
-            <React.Fragment key={item.name}>
-              <IconButton
-                to={item.path}
-                key={item.name}
-                component={Link}
-                sx={{ display: 'inline-block' }}
-                color="inherit"
-              >
-                {item.icon}
-              </IconButton>
-            </React.Fragment>
+            <BottomNavigationAction
+              key={item.name}
+              label={item.name}
+              value={item.path}
+              sx={{ minWidth: 0, flex: 1 }}
+              icon={
+                item.badge ? (
+                  <Badge
+                    badgeContent={item.badge.count}
+                    color={item.badge.color}
+                  >
+                    {item.icon}
+                  </Badge>
+                ) : (
+                  item.icon
+                )
+              }
+              component={Link}
+              to={item.path}
+            />
           ))}
-      </Toolbar>
-    </AppBar>
+      </BottomNavigation>
+    </Paper>
   );
 };
