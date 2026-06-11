bbfb777231ba325a5bb7d1d0da7e1cfa6d81300a
Richard Martija <richardmartija@gmail.com>
2026-06-11 20:55:15 +0000

    fix(server): properly exclude season 0 of a show when that season is on the exclude list Fixes #1914

--- server/src/services/scheduling/slotSchedulerUtil.ts
+++ server/src/services/scheduling/slotSchedulerUtil.ts
@@ -570,7 +570,7 @@ function getContentProgramIterator(
     if (slot.seasonExcludeFilter?.length > 0) {
       programs = programs.filter((program) => {
         const season = program.season?.index ?? program.seasonNumber;
-        return !season || !slot.seasonExcludeFilter.includes(season);
+        return season == null || !slot.seasonExcludeFilter.includes(season);
       });
     }
   }
